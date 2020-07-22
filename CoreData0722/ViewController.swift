1a77c16dd5e1f62799666ef199c13bb45202b3c5
Leslie <leslie.appl@gmail.com>
2020-07-22 12:02:18 +0800

    Initial Commit

---
+++ CoreData0722/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData0722
+//
+//  Created by leslie on 7/22/20.
+//  Copyright © 2020 leslie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
